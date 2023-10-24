76c1af63fc6a119ef50f58f6b7bd0f34b457027b
FIDEL CASTRO <okanga.fidel@gmail.com>
2023-10-24 13:42:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-<script>
-    // Initialize cart count
-    let cartCount = 0;
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
 
-    // Function to update the cart count
-    function updateCartCount() {
-        document.getElementById("cart-count").textContent = cartCount;
-    }
-
-    // Click event for the "Add to Cart" button
-    const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
-    addToCartButtons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            // Prevent the default behavior of the anchor tag
-            event.preventDefault();
-
-            cartCount++;
-            updateCartCount();
-        });
-    });
-</script>
